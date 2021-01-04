537eb162c9cd1519df6945e7e9d6787816b7a1d6
Washington Soares <washingtonudi@gmail.com>
2021-01-04 08:42:05 -0300

    Update OrderLocation.tsx

--- aula2/Orders/OrderLocation.tsx
+++ aula2/Orders/OrderLocation.tsx
@@ -1,27 +1,27 @@
 const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
-    const response = await fetchLocalMapBox(inputValue);
+  const response = await fetchLocalMapBox(inputValue);
 
-    const places = response.data.features.map((item: any) => {
-      return ({
-        label: item.place_name,
-        value: item.place_name,
-        coords: item.center,
-        place: item.place_name,
-      });
+  const places = response.data.features.map((item: any) => {
+    return ({
+      label: item.place_name,
+      value: item.place_name,
+      coords: item.center,
+      place: item.place_name,
     });
+  });
 
-    callback(places);
-  };
+  callback(places);
+};
 
-  const handleChangeSelect = (place: Place) => {
-    setAddress(place);
-    setLocation({
-      lng: place.coords[0],
-      lat: place.coords[1],
-    });
-    onChangeLocation({
-      latitude: place.coords[1],
-      longitude: place.coords[0],
-      address: place.label
-    });
-  };
+const handleChangeSelect = (place: Place) => {
+  setAddress(place);
+  setLocation({
+    lng: place.coords[0],
+    lat: place.coords[1],
+  });
+  onChangeLocation({
+    latitude: place.coords[1],
+    longitude: place.coords[0],
+    address: place.label
+  });
+};
