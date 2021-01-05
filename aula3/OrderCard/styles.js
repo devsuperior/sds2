2f3bfcb3fedbd6b2d26d1059d08a57cf59533297
Washington Soares <washingtonudi@gmail.com>
2021-01-05 17:20:57 -0300

    Create styles.js

---
+++ aula3/OrderCard/styles.js
@@ -0,0 +1,47 @@
+{
+  container: {
+    marginTop: '10%',
+    padding: 15,
+    backgroundColor: '#FFF',
+    shadowOpacity: 0.25,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 20,
+    borderRadius: 10
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontWeight: 'normal',
+    fontSize: 14,
+    lineHeight: 19,
+    letterSpacing: -0.24,
+    color: '#9E9E9E',
+    fontFamily: 'OpenSans_400Regular'
+  },
+  orderName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#263238',
+    fontFamily: 'OpenSans_700Bold'
+  },
+  orderPrice: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    textAlign: 'right',
+    letterSpacing: -0.24,
+    color: '#DA5C5C',
+    fontFamily: 'OpenSans_700Bold'
+  },
+  productsList: {
+    borderTopColor: '#E6E6E6',
+    borderTopWidth: 1,
+    marginTop: 20,
+    paddingTop: 15
+  }
+}
