bbc166550a514a801465b8de932e0299c525fe2c
Washington Soares <washingtonudi@gmail.com>
2021-01-05 10:05:34 -0300

    Create styles.js

---
+++ aula3/Home/styles.js
@@ -0,0 +1,44 @@
+{
+  container: {
+    marginTop: '15%',
+    alignItems: 'center'
+  },
+  gamerImage: {
+    width: 292,
+    height: 229
+  },
+  title: {
+    color: '#263238',
+    fontSize: 26,
+    lineHeight: 35,
+    fontWeight: 'bold',
+    marginTop: 31,
+    textAlign: 'center'
+  },
+  subTitle: {
+    color: '#9E9E9E',
+    fontSize: 16,
+    marginTop: 15,
+    lineHeight: 22,
+    textAlign: 'center'
+  },
+  footer: {
+    marginTop: '15%',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#DA5C5C',
+    flexDirection: 'row',
+    borderRadius: 10
+  },
+  buttonText: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF',
+    letterSpacing: -0.24
+  }
+}
