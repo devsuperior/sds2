5aadb17ec8c297558954e78b5acb79e1b6c35b10
Washington Soares <washingtonudi@gmail.com>
2021-01-05 20:04:45 -0300

    Create styles.ts

---
+++ aula3/OrderDetails/styles.ts
@@ -0,0 +1,25 @@
+{
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%'
+  },
+  button: {
+    backgroundColor: '#DA5C5C',
+    flexDirection: 'row',
+    borderRadius: 10,
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF',
+    letterSpacing: -0.24,
+    fontFamily: 'OpenSans_700Bold'
+  }
+}
