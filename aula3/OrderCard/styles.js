45193b34053f183421e7c58aed390862adcea17f
Washington Soares <washingtonudi@gmail.com>
2021-01-07 21:28:48 -0300

    Update styles.js

--- aula3/OrderCard/styles.js
+++ aula3/OrderCard/styles.js
@@ -1,13 +1,17 @@
 {
   container: {
     marginTop: '10%',
+    marginLeft: '2%',
+    marginRight: '2%',
+    marginBottom: '2%',
     padding: 15,
     backgroundColor: '#FFF',
     shadowOpacity: 0.25,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 20,
-    borderRadius: 10
+    borderRadius: 10,
+    elevation: 5
   },
   header: {
     flexDirection: 'row',
