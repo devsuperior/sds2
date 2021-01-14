a98fe4b777256e0c60203070dbfc310fcfd77cef
AmjadNoor <56997478+AmjadNoor@users.noreply.github.com>
2021-01-14 18:22:32 +0530

    Update Styles.js

--- aula3/Home/styles.js
+++ aula3/Home/styles.js
@@ -1,40 +1,40 @@
 {
   container: {
-    marginTop: '5%',
-    alignItems: 'center'
-  },
+    marginTop: '5%';
+    alignItems: 'center';
+  }
   title: {
-    color: '#263238',
-    fontSize: 26,
-    lineHeight: 35,
-    fontWeight: 'bold',
-    marginTop: 31,
-    textAlign: 'center'
-  },
+    color: '#263238';
+    fontSize: 26;
+    lineHeight: 35;
+    fontWeight: 'bold';
+    marginTop: 31;
+    textAlign: 'center';
+  }
   subTitle: {
-    color: '#9E9E9E',
-    fontSize: 16,
-    marginTop: 15,
-    lineHeight: 22,
-    textAlign: 'center'
-  },
+    color: '#9E9E9E';
+    fontSize: 16;
+    marginTop: 15;
+    lineHeight: 22;
+    textAlign: 'center';
+  }
   footer: {
-    marginTop: '5%',
-    alignItems: 'center'
-  },
+    marginTop: '5%';
+    alignItems: 'center';
+  }
   button: {
-    backgroundColor: '#DA5C5C',
-    flexDirection: 'row',
-    borderRadius: 10
-  },
+    backgroundColor: '#DA5C5C';
+    flexDirection: 'row';
+    borderRadius: 10;
+  }
   buttonText: {
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 50,
-    paddingRight: 50,
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#FFF',
-    letterSpacing: -0.24
+    paddingTop: 15;
+    paddingBottom: 15;
+    paddingLeft: 50;
+    paddingRight: 50;
+    fontWeight: 'bold';
+    fontSize: 18;
+    color: '#FFF';
+    letterSpacing: -0.24;
   }
 }
